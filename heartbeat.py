6b6133a9ecfa63da9db7091a881cdd5c4dfef679
Franklin Reynolds <franklin.reynolds@gmail.com>
2020-08-12 16:21:41 -0400

    add import sys and fix repetition loop

--- heartbeat.py
+++ heartbeat.py
@@ -13,6 +13,7 @@
 import requests
 import time
 import argparse
+import sys
 
 def forward():
     #r = requests.post('http://localhost:5000/forward', data = {'key': 'value'})
@@ -23,13 +24,13 @@ def cliparse(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('-r', '--repetitions', type=int, default=1, help="N==repeat N times; -1 repeat forever")
     parser.add_argument('-s', '--sleep', type=int, default=1, choices=range(1,60), help="N==number of seconds to sleep between heartbeats")
-    return args
+    return parser.parse_args()
     
 def main():
     args = cliparse(sys.argv[1:])
     sleep = args.sleep
     repetitions = args.repetitions
-    count = 0
+    count = 1
 
     while True:
         forward()
@@ -37,9 +38,8 @@ def main():
         if repetitions == -1:
             continue
         count += 1
-        if count < repetitions:
-            continue
-
+        if count > repetitions:
+            break
 
 if __name__ == "__main__":
     main()
